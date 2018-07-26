c6f8669516700bc5807ca32d4543fcf004f3fa70
jfqd <jfqd@blun.org>
2018-07-26 23:18:30 +0200

    do not create dh4096

--- copy/var/zoneinit/includes/42-postfix.sh
+++ copy/var/zoneinit/includes/42-postfix.sh
@@ -1,5 +1,5 @@
 # change dh-files on every provisioning
-openssl dhparam -out /opt/local/etc/postfix/dh4096.pem 4096
+# openssl dhparam -out /opt/local/etc/postfix/dh4096.pem 4096
 openssl dhparam -out /opt/local/etc/postfix/dh2048.pem 2048
 openssl dhparam -out /opt/local/etc/postfix/dh1024.pem 1024
 openssl dhparam -out /opt/local/etc/postfix/dh512.pem 512
