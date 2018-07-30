4ac2d8b321397ffc3e38d6625e17ebc04ed6161d
jfqd <jfqd@blun.org>
2018-07-30 10:21:09 +0200

    fix build image

--- copy/opt/qutic/var/mdata-setup/includes/32-policyd.sh
+++ copy/opt/qutic/var/mdata-setup/includes/32-policyd.sh
@@ -1,11 +1,11 @@
 if mdata-get postfix_mysqluser 1>/dev/null 2>&1; then
   MYSQL_USER=`mdata-get postfix_mysqluser`
-  sed -i "s/Username=\"postfix\"/Username=\"${MYSQL_USER}\"/" /opt/local/etc/cluebringer.conf
+  sed -i "s/Username=postfix/Username=${MYSQL_USER}/" /opt/local/etc/cluebringer.conf
 fi
 
 if mdata-get postfix_mysqlpassword 1>/dev/null 2>&1; then
   MYSQL_PWD=`mdata-get postfix_mysqlpassword`
-  sed -i "s/Password=\"p0stf1x\"/Password=\"${MYSQL_PWD}\"/" /opt/local/etc/cluebringer.conf
+  sed -i "s/Password=p0stf1x/Password=${MYSQL_PWD}/" /opt/local/etc/cluebringer.conf
 fi
 
 svcadm enable svc:/network/policyd:default
