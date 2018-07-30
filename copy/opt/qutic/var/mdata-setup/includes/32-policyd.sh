c212e9c4344b41a73a32c3cafb190ebc019a3f62
jfqd <jfqd@blun.org>
2018-07-30 08:47:50 +0200

    switch to latest policyd v2.1

--- copy/opt/qutic/var/mdata-setup/includes/32-policyd.sh
+++ copy/opt/qutic/var/mdata-setup/includes/32-policyd.sh
@@ -1,14 +1,14 @@
 if mdata-get postfix_mysqluser 1>/dev/null 2>&1; then
   MYSQL_USER=`mdata-get postfix_mysqluser`
-  sed -i "s/MYSQLUSER=\"postfix\"/MYSQLUSER=\"${MYSQL_USER}\"/" /opt/local/etc/policyd.conf
+  sed -i "s/Username=\"postfix\"/Username=\"${MYSQL_USER}\"/" /opt/local/etc/cluebringer.conf
 fi
 
 if mdata-get postfix_mysqlpassword 1>/dev/null 2>&1; then
   MYSQL_PWD=`mdata-get postfix_mysqlpassword`
-  sed -i "s/MYSQLPASS=\"p0stf1x\"/MYSQLPASS=\"${MYSQL_PWD}\"/" /opt/local/etc/policyd.conf
+  sed -i "s/Password=\"p0stf1x\"/Password=\"${MYSQL_PWD}\"/" /opt/local/etc/cluebringer.conf
 fi
 
 svcadm enable svc:/network/policyd:default
 
-CRON='0 * * * * /opt/local/libexec/policyd/cleanup -c /opt/local/etc/policyd.conf'
+CRON='0 * * * * /opt/local/libexec/cbpadmin --cleanup'
 (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
