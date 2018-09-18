c8396ece0741607d96e2b3415b2153c79fd7cf5b
jfqd <jfqd@blun.org>
2018-09-18 10:50:25 +0200

    add my.cnf for mysql-check coming later

--- copy/opt/qutic/var/mdata-setup/includes/35-spiped.sh
+++ copy/opt/qutic/var/mdata-setup/includes/35-spiped.sh
@@ -14,3 +14,13 @@ if mdata-get spiped_percona_host 1>/dev/null 2>&1; then
   sed -i "s/-t 127.0.0.1:23306/-t $SPIPED_PERCONA_HOST:23306/g" /opt/local/lib/svc/manifest/spiped-percona.xml
   svccfg import /opt/local/lib/svc/manifest/spiped-percona.xml
 fi
+
+if mdata-get postfix_mysqluser 1>/dev/null 2>&1; then
+  MYSQLUSER=`mdata-get postfix_mysqluser`
+  sed -i "s/user = postfix_mysqluser/user = $MYSQLUSER/" /root/.my.cnf
+fi
+
+if mdata-get postfix_mysqlpassword 1>/dev/null 2>&1; then
+  MYSQLPWD=`mdata-get postfix_mysqlpassword`
+  sed -i "s/password = postfix_mysqlpassword/password = $MYSQLPWD/" /root/.my.cnf
+fi
