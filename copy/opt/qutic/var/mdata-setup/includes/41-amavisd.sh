6e148533315b3d9408c9f6803900634a9ab75c5d
jfqd <jfqd@blun.org>
2018-12-15 12:43:09 +0100

    use mysql backend for bayes

--- copy/opt/qutic/var/mdata-setup/includes/41-amavisd.sh
+++ copy/opt/qutic/var/mdata-setup/includes/41-amavisd.sh
@@ -2,3 +2,8 @@ if mdata-get postfix_postmaster 1>/dev/null 2>&1; then
   DOMAIN=`mdata-get postfix_postmaster | awk -F @ {'print$2'}`
   sed -i "s/$mydomain = 'example.com'/$mydomain = '${DOMAIN}'/" /opt/local/etc/amavisd.conf
 fi
+
+if mdata-get postfix_mysqlpassword 1>/dev/null 2>&1; then
+  MYSQLPWD=`mdata-get postfix_mysqlpassword`
+  sed -i "s/bayes_sql_password <sa_password>/bayes_sql_password ${MYSQLPWD}/" /opt/local/etc/spamassassin/local.cf
+fi
