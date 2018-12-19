f1d926b1c9f3effbd702b949bcb1bfa8cc1c008b
jfqd <jfqd@blun.org>
2018-12-19 10:39:38 +0100

    change cron time

--- copy/var/zoneinit/includes/33-spamd.sh
+++ copy/var/zoneinit/includes/33-spamd.sh
@@ -2,8 +2,8 @@
 sudo -u spamd /opt/local/bin/sa-update
 
 # create cronjob for sa-update
-CRON='35 7 * * * sudo -u spamd /opt/local/bin/sa-update --nogpg --channel spamassassin.heinlein-support.de
-42 7 * * * sudo -u spamd /opt/local/bin/sa-update ; /usr/sbin/svcadm restart svc:/pkgsrc/amavisd:default'
+CRON='35 4 * * * sudo -u spamd /opt/local/bin/sa-update --nogpg --channel spamassassin.heinlein-support.de
+42 4 * * * sudo -u spamd /opt/local/bin/sa-update ; /usr/sbin/svcadm restart svc:/pkgsrc/amavisd:default'
 (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
 
 # Run pyzor discover
