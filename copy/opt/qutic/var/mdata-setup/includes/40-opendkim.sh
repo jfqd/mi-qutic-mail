9b6c4b692aab166a9b8a69779c35679d5eefe754
jfqd <jfqd@blun.org>
2019-03-26 20:44:06 +0100

    add opendkim trusted_hosts

--- copy/opt/qutic/var/mdata-setup/includes/40-opendkim.sh
+++ copy/opt/qutic/var/mdata-setup/includes/40-opendkim.sh
@@ -3,3 +3,9 @@ if mdata-get postfix_hostname 1>/dev/null 2>&1; then
   sed -i "s/# AuthservID\t\texample.com/AuthservID\t\t${MYHOSTNAME}/" /opt/local/etc/opendkim.conf
   /usr/sbin/svcadm enable -r svc:/pkgsrc/opendkim:default
 fi
+
+if mdata-get postfix_mynetworks 1>/dev/null 2>&1; then
+  for host in `mdata-get postfix_mynetworks | sed  's/,/ /g'` ; do
+    echo $host >> /var/db/opendkim/trusted_hosts
+  done
+fi
