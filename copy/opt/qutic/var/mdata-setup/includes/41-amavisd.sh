28c613cb11338385178a7fabfa3bad66e2b57f1a
jfqd <jfqd@blun.org>
2019-03-26 10:10:07 +0100

    set @mynetworks-list for amavis and trusted_networks for spamassasin

--- copy/opt/qutic/var/mdata-setup/includes/41-amavisd.sh
+++ copy/opt/qutic/var/mdata-setup/includes/41-amavisd.sh
@@ -1,9 +1,27 @@
 if mdata-get postfix_postmaster 1>/dev/null 2>&1; then
   DOMAIN=`mdata-get postfix_postmaster | awk -F @ {'print$2'}`
-  sed -i "s/$mydomain = 'example.com'/$mydomain = '${DOMAIN}'/" /opt/local/etc/amavisd.conf
+  sed -i \
+      "s/$mydomain = 'example.com'/$mydomain = '${DOMAIN}'/" \
+      /opt/local/etc/amavisd.conf
+fi
+
+if mdata-get postfix_mynetworks 1>/dev/null 2>&1; then
+  MYNETWORK_LIST=`mdata-get postfix_mynetworks | sed  's/,/ /g'`
+  sed -i \
+      "s/@mynetworks = qw( 127.0.0.0/8 192.168.0.0/16 );/@mynetworks = qw( ${MYNETWORK_LIST} );/" \
+      /opt/local/etc/amavisd.conf
+fi
+
+if mdata-get postfix_mynetworks 1>/dev/null 2>&1; then
+  MYNETWORK=`mdata-get postfix_mynetworks`
+  sed -i \
+      "s/# trusted_networks 212.17.35./trusted_networks ${MYNETWORK}/" \
+      /opt/local/etc/spamassassin/local.cf
 fi
 
 if mdata-get postfix_mysqlpassword 1>/dev/null 2>&1; then
   MYSQLPWD=`mdata-get postfix_mysqlpassword`
-  sed -i "s/bayes_sql_password <sa_password>/bayes_sql_password ${MYSQLPWD}/" /opt/local/etc/spamassassin/local.cf
+  sed -i \
+      "s/bayes_sql_password <sa_password>/bayes_sql_password ${MYSQLPWD}/" \
+      /opt/local/etc/spamassassin/local.cf
 fi
