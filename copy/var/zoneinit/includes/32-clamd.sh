190fba0d4689398c8a5b0712ecf7f943bbd3ae5a
jfqd <jfqd@blun.org>
2019-04-23 09:26:44 +0200

    skip clamav-unofficial for now

--- copy/var/zoneinit/includes/32-clamd.sh
+++ copy/var/zoneinit/includes/32-clamd.sh
@@ -1,5 +1,5 @@
-CRON='15 * * * * sudo -u clamav /opt/local/bin/clamav-unofficial-sigs > /dev/null'
-(crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
+# CRON='15 * * * * sudo -u clamav /opt/local/bin/clamav-unofficial-sigs > /dev/null'
+# (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
 
 # run freshclamd once on provisioning
 /opt/local/bin/freshclam
