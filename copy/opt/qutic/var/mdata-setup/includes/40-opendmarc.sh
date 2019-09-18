3d6a9f36d4a1b11062dc82b60718a1fef934ee05
jfqd <jfqd@blun.org>
2019-09-18 12:02:57 +0200

    add cron for dmarc cleanup

--- copy/opt/qutic/var/mdata-setup/includes/40-opendmarc.sh
+++ copy/opt/qutic/var/mdata-setup/includes/40-opendmarc.sh
@@ -37,6 +37,9 @@ if mdata-get opendmarc_reporter 1>/dev/null 2>&1; then
   if [ "`mdata-get opendmarc_reporter`" = "true" ]; then
     CRON='15 3 * * * sudo -u opendmarc /opt/qutic/bin/opendmarc-reporter &>>/var/log/opendmarc/reporter.log'
     (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
+    
+    CRON='15 5 * * * sudo -u vmail /opt/qutic/bin/cleanup_dmarc_errors 1>/dev/null'
+    (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
   fi
 fi
 
