2bd57d5317f37ea212e7e819c795fb5c6ddf5f22
jfqd <jfqd@blun.org>
2020-03-01 11:53:33 +0100

    install zabbix-postfix with requirements

--- copy/opt/qutic/var/mdata-setup/includes/31-cron.sh
+++ copy/opt/qutic/var/mdata-setup/includes/31-cron.sh
@@ -8,3 +8,7 @@ fi
 # prevent logadm from going wild
 CRON='5 * * * * [[ `/usr/bin/ps auxwww | /usr/bin/grep logadm | /usr/bin/wc -l` -gt 5 ]] && /usr/bin/pkill logadm'
 (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
+
+# send stats to zabbix
+CRON="0,5,10,15,20,25,30,35,40,45,50,55 * * * * sudo -u zabbix /opt/local/bin/zabbix_postfix 1>/dev/null 2>/dev/null"
+(crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
